1f68ad01ccb536be46723f413e109032f5e613cd
brian <bfvilchez95@gmail.com>
2020-02-28 09:59:09 -0700

    linked View controller and collectionView Controller to storyboard files

---
+++ Pokedex/Pokedex/View Controllers/PokemonsCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  PokemonsCollectionViewController.h
+//  Pokedex
+//
+//  Created by brian vilchez on 2/28/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PokemonsCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
